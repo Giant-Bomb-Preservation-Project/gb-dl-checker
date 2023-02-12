a2f574cf36c26c830f4ff6ae1a40b3b3351fccf7
muffinsAKA <muffins@gmail.com>
2023-02-12 02:38:22 -0500

    fixed handling of spaces in upload path names

--- gb-dl-checker.py
+++ gb-dl-checker.py
@@ -274,16 +274,27 @@ while True:
         # Save sg.Multiline console window to text file
         with open("LogFile.txt", "w", encoding='UTF-8') as f:
           f.write(window['logfile'].get())
+    
+    # Upload to Internet Archive (must be logged in)
     elif event == "Upload":
-      outpath_manager = f'{os.path.splitext(output_csv)[0]}_upload_manager.bat'
+      
+      # Creates upload launcher naming based on Upload CSV input
+      outpath_manager = f'{os.path.splitext(output_csv)[0]}_upload_manager.ps1'
+      
+      # Deletes existing launcher
       if os.path.exists(outpath_manager):
         os.remove(outpath_manager)
+      
+      # Write separate Powershell scripts for each CSV created earlier
+      # e.g. MarioMaker1.csv, Mariomaker2.csv would mean MarioMaker1.ps1, Mariomaker2.ps1
       for i in range(output_parts):
-        outpath_bat = f'{os.path.splitext(output_csv)[0]}{i+1}.bat' if output_parts > 1 else f'{os.path.splitext(output_csv)[0]}.bat'       
+        outpath_bat = f'{os.path.splitext(output_csv)[0]}{i+1}.ps1' if output_parts > 1 else f'{os.path.splitext(output_csv)[0]}.bat'       
         outpath_csv = f'{os.path.splitext(output_csv)[0]}{i+1}.csv' if output_parts > 1 else output_csv
         with open(outpath_bat, "w", encoding='UTF-8') as f:
-            f.write('ia upload --spreadsheet=' + outpath_csv)
+            f.write('ia upload --spreadsheet=' + '\"' + outpath_csv + '\"' + ' --retries 100')
             f.close
+        
+        # Write upload launcher PS1 file that executes each individual instance of Powershell
         with open(outpath_manager, "a", encoding='UTF-8') as f:
-          f.write(f"start {outpath_bat} \n")
-      subprocess.Popen([outpath_manager],shell=True)
+          f.write(f"start \"{outpath_bat}\" \n")
+      subprocess.Popen([outpath_manager], shell=True)
